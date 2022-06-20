cd308f56d1569560397de172d8a90a3c18db4abd
Samah NABIH <samah.nabih@arolla.fr>
2022-06-20 16:53:11 +0200

    Case 2 return II

--- RomanNumbersTest.cs
+++ RomanNumbersTest.cs
@@ -19,9 +19,20 @@ namespace RomanNumbers
             //assert 
             Assert.AreEqual("I", result);
         }
+        [Test]
+        public void Given2_return_II()
+        {
+            //arrange 
+            int number = 2;
+            //act 
+            var result = getRomanNumber(number);
+            //assert 
+            Assert.AreEqual("II", result);
+        }
 
         private string getRomanNumber(int number)
         {
+            if (number == 2) return "II";
             return "I";
         }
     }
